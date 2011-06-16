2033e483bc97395c051f81742de689d4af7ee510
Mary Benn <mbenn@wellesley.edu>
2011-06-16 15:35:17 -0400

    foo

---
+++ test1.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+<form>
+<input type="button"
+     value="Guess what?" 
+     name="button1"
+     onClick=alert"It's a button that does stuff!">
+</form>
+</body>
+</html>
